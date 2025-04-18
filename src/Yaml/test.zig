515c1fba5503d8947828c1533d2e2f21f97738c4
Andrew Hart <andrew@achart.me.uk>
2025-04-18 22:30:34 +0100

    Support enum types

--- src/Yaml/test.zig
+++ src/Yaml/test.zig
@@ -113,6 +113,32 @@ test "several integer bases" {
     try testing.expectEqualSlices(i8, &[_]i8{ 10, -10, 16, -16, 8, -8 }, &arr);
 }
 
+test "enums" {
+    const TestEnum = enum {
+        alpha,
+        beta,
+        charlie,
+    };
+
+    const source =
+        \\- alpha
+        \\- beta
+        \\- charlie
+    ;
+
+    var yaml: Yaml = .{ .source = source };
+    defer yaml.deinit(testing.allocator);
+    try yaml.load(testing.allocator);
+
+    try testing.expectEqual(yaml.docs.items.len, 1);
+
+    var arena = Arena.init(testing.allocator);
+    defer arena.deinit();
+
+    const arr = try yaml.parse(arena.allocator(), [3]TestEnum);
+    try testing.expectEqualSlices(TestEnum, &[_]TestEnum{ .alpha, .beta, .charlie }, &arr);
+}
+
 test "simple map untyped" {
     const source =
         \\a: 0
@@ -640,4 +666,16 @@ test "stringify a list" {
 
     const arr: [3]i64 = .{ 1, 2, 3 };
     try testStringify("[ 1, 2, 3 ]", arr);
+}
+
+test "stringify an enum" {
+    const TestEnum = enum {
+        alpha,
+        beta,
+        charlie,
+    };
+
+    try testStringify("alpha", TestEnum.alpha);
+    try testStringify("beta", TestEnum.beta);
+    try testStringify("charlie", TestEnum.charlie);
 }
